9cda5f7675e1ac3bc08ad861737c367a2dd36d9d
khanage <khanage@gmail.com>
2025-08-16 12:31:23 +1000

    Add direnv and update to use flake

--- flake.nix
+++ flake.nix
@@ -7,19 +7,26 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
     flake-utils.lib.eachDefaultSystem
-     (system:
-      let
-        overlays = [ (import rust-overlay) ];
+    (
+      system: let
+        overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
         nativeBuildInputs = with pkgs; [
           rust-bin.stable.latest.default
           pkg-config
+          lld
+          clang
         ];
-        buildInputs = with pkgs; [ 
+        buildInputs = with pkgs; [
           udev
           wayland
           alsa-lib
@@ -27,24 +34,12 @@
           vulkan-loader
         ];
       in
-      with pkgs;
-      {
-          devShells.default = with pkgs; mkShell {
-            inherit nativeBuildInputs buildInputs;
-
-            shellHook = ''
-              export LD_LIBRARY_PATH=${
-                pkgs.lib.makeLibraryPath [ 
-                  udev
-                  alsa-lib
-                  vulkan-loader
-                  wayland
-                  libxkbcommon
-                ]}
-            
-              echo 🎯 Setting LD_LIBRARY_PATH=$LD_LIBRARY_PATH
-            '';
-          };
+        with pkgs; {
+          devShells.default = with pkgs;
+            mkShell {
+              inherit nativeBuildInputs buildInputs;
+              LD_LIBRARY_PATH = builtins.foldl' (a: b: "${a}:${b}/lib") "${pkgs.vulkan-loader}/lib" buildInputs;
+            };
         }
-     );
+    );
 }
